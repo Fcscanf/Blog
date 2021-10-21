a99ac4886bf4edf6fcd9b7d491e1a00cc3b1ffc3
Fcant <fcscanf@outlook.com>
2021-10-21 20:08:53 +0800

    :tada: Init Pro

---
+++ src/main/java/com/fcant/blog/BlogApplication.java
@@ -0,0 +1,21 @@
+package com.fcant.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, IOException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
